990f8bca9d0abcb2a3339cccf8e7c23f9f279843
Vinit Kanani <vinitkanani@Vinits-MacBook-Pro.local>
2022-11-13 14:50:42 -0800

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react"],
+  rules: {
+    "@typescript-eslint/quotes": 0,
+    "@typescript-eslint/semi": 0,
+  },
+};
